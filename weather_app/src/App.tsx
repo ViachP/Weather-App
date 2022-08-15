602384121c23b9e76c83dae39cd11e5dd7c22821
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-15 23:20:36 +0300

    add pages routing

--- weather_app/src/App.tsx
+++ weather_app/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
 import "./styles/index.scss";
 
 function App() {
-  return <div className="App">Привет</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/MonthStatistics" element={<MonthStatistics />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
