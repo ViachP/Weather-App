0ec9ddb3be722c66ac9f590e8d747a789809efc5
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-18 23:29:49 +0300

    fix sass issues

--- weather_app/src/App.tsx
+++ weather_app/src/App.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
-import "./styles/index.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/MonthStatistics" element={<MonthStatistics />} />
-      </Routes>
-    </div>
-  );
+  return <div className="App">Home page</div>;
 }
 
 export default App;
--- weather_app/src/index.tsx
+++ weather_app/src/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "normalize.css";
 import "./styles/index.scss";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
