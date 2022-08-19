6f3ac42b3e738673d37011eb6870e0dae76691fe
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-19 22:42:30 +0300

    fix issues with routing

--- weather_app/src/App.tsx
+++ weather_app/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
 
 function App() {
-  return <div className="App">Home page</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/MonthStatistics" element={<MonthStatistics />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
