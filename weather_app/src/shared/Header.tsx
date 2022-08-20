b53765c9bf0a8ab67f184a0b5d94b535f2c501e4
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-20 21:29:43 +0300

    add styles for Header

--- weather_app/src/shared/Header.tsx
+++ weather_app/src/shared/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import s from "./Header.module.scss";
 import { GlobalSvgSelector } from "../assets/icons/global/GlobalSvgSelector";
+import Select from "react-select";
 
 interface Props {}
 
 export const Header = (props: Props) => {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
@@ -13,7 +20,12 @@ export const Header = (props: Props) => {
         </div>
         <div className={s.title}>Weather App</div>
       </div>
-      <div className={s.wrapper}></div>
+      <div className={s.wrapper}>
+        <div className={s.change_theme}>
+          <GlobalSvgSelector id="change-theme" />
+        </div>
+        <Select options={options} />
+      </div>
     </header>
   );
 };
