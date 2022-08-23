9006b98efa816f2d34f441f658e6787956aee5b7
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-23 20:57:51 +0300

    add variables for background,shadow and text

--- weather_app/src/pages/Home/components/Days/Tabs.tsx
+++ weather_app/src/pages/Home/components/Days/Tabs.tsx
@@ -24,7 +24,7 @@ export const Tabs = (props: Props) => {
           </div>
         ))}
       </div>
-      <div className={s.cancel}>Отменить</div>
+      <div className={s.cancel}>Cancel</div>
     </div>
   );
 };
