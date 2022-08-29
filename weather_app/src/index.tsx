64ec722bea17ee9f7db5706015de70cf774774e4
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-08-29 20:33:35 +0300

    add slice for redux-toolkit

--- weather_app/src/index.tsx
+++ weather_app/src/index.tsx
@@ -4,16 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./provider/ThemeProvider";
 import App from "./App";
 import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
